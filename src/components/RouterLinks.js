7486c34087a517a9d5bccb7be40f9b594d91ff14
AlhexBlack <michealunuafeh@gmail.com>
2024-08-22 09:44:13 +0100

    active tabs added successfully

--- src/components/RouterLinks.js
+++ src/components/RouterLinks.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
@@ -11,6 +11,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLock } from '@fortawesome/free-solid-svg-icons'
 
 const RouterLinks = () => {
+    const [selectedLink, setSelectedLink] = useState(null);
+
+    const handleLinkClick = (linkText) => {
+        setSelectedLink(linkText);
+    };
+
   return (
     <Router>
         <div>
@@ -18,32 +24,46 @@ const RouterLinks = () => {
                 <ul id='navul'>
                     <li>
                         <div id='item1'>
-                            <Link to="/">Muzic</Link>
+                            <Link to="/"
+                            className={selectedLink === 'Home' ? 'active' : ''} onClick={() => handleLinkClick('Home')}
+                            >Muzic</Link>
                         </div>
                     </li>    
                     <li>
                         <div id='item2'>
                             <li>
-                                <Link to="/" id='item2a'>Home</Link>
+                                <Link to="/" id='item2a'
+                                className={selectedLink === 'Home' ? 'active' : ''} onClick={() => handleLinkClick('Home')}
+                                >Home</Link>
                             </li>
                             <li>
-                                <Link to="/about" id='item2b'>About</Link>
+                                <Link to="/about" id='item2b'
+                                className={selectedLink === 'About' ? 'active' : ''} onClick={() => handleLinkClick('About')}
+                                >About</Link>
                             </li>
                             <li>
-                                <Link to="/features" id='item2c'>Features</Link>
+                                <Link to="/features" id='item2c'
+                                className={selectedLink === 'Features' ? 'active' : ''} onClick={() => handleLinkClick('Features')}
+                                >Features</Link>
                             </li>
                             <li>
-                                <Link to="/contact" id='item2d'>Contact</Link>
+                                <Link to="/contact" id='item2d'
+                                className={selectedLink === 'Contact' ? 'active' : ''} onClick={() => handleLinkClick('Contact')}
+                                >Contact</Link>
                             </li>
                         </div>
                     </li>
                     <li>
                         <div id='item3'>
                             <li>
-                            <button id='signinbtn'><Link to="/signup"><FontAwesomeIcon icon={faLock} id='lock'/></Link></button>
+                            <button id='signinbtn'
+                            className={selectedLink === 'SignIn' ? 'activebtn' : ''} onClick={() => handleLinkClick('SignIn')}
+                            ><Link to="/signin"><FontAwesomeIcon icon={faLock} id='lock'/></Link></button>
                             </li> 
                             <li>                
-                            <button id='signupbtn'><Link to="/signin">SignUp</Link></button>
+                            <button id='signupbtn'
+                            className={selectedLink === 'SignUp' ? 'activebtn' : ''} onClick={() => handleLinkClick('SignUp')}
+                            ><Link to="/signup" id='lock'>SignUp</Link></button>
                             </li>   
                         </div>
                     </li>    
